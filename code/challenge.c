fad0b152f9ded7cc23c10bb94fc506aa4550371b
Christopher Tan <72955765+Christopher-Tan@users.noreply.github.com>
2022-05-27 15:04:25 -0400

    Testing binary file on other OS's

--- code/challenge.c
+++ code/challenge.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-	char a[] = {'t','e','s','t','\0','f','l','a','g','{','r','e','s','t','}','\0'};
+	char a[] = {'t', 'e', 's', 't', '\0', 'f', 'l', 'a', 'g', '{', 'b', 'u', 'f', 'f', '3', 'r', '0', 'v', '3', 'r', 'f', 'l', '0', 'w', '}', '\0'};
 	char b[492] = {0};
 	read(STDIN_FILENO, b, 1000);
 	printf(a);
